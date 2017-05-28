7e1582c1174973be62fe80084d94c7c216f0e205
DeGuitard <wired@n4v1.com>
2017-05-28 19:27:17 +0200

    Added second book and intermediary page.

--- server/index.js
+++ server/index.js
@@ -9,12 +9,15 @@ app.set('views', __dirname + '/../client/views');
 app.set('view engine', 'ejs');
 
 // Registering views
-app.get('/',                        function(req, res) { res.render('index',                    { page: 'index',    title: 'Poésie de l\'instant'}); });
-app.get('/livre',                   function(req, res) { res.render('livre',                    { page: 'livre',    title: 'Livre' }); });
-app.get('/ocean',                   function(req, res) { res.render('ocean',                    { page: 'ocean',    title: 'Océan' }); });
-app.get('/foret',                   function(req, res) { res.render('foret',                    { page: 'foret',    title: 'Forêt' }); });
-app.get('/chevaux',                 function(req, res) { res.render('chevaux',                  { page: 'chevaux',  title: 'Chevaux' }); });
-app.get('/contact',                 function(req, res) { res.render('contact',                  { page: 'contact',  title: 'Contact' }); });
+app.get('/',                        function(req, res) { res.render('index',                    { page: 'index',        title: 'Poésie de l\'instant'}); });
+app.get('/livres',                  function(req, res) { res.render('livres',                   { page: 'livres',       title: 'Livres' }); });
+app.get('/livre',                   function(req, res) { res.render('surfeuses-bout-du-monde',  { page: 'surfeuses',    title: 'Surfeuses du bout du monde' }); });
+app.get('/surfeuses-bout-du-monde', function(req, res) { res.render('surfeuses-bout-du-monde',  { page: 'surfeuses',    title: 'Surfeuses du bout du monde' }); });
+app.get('/matin-des-arbres',        function(req, res) { res.render('matin-des-arbres',         { page: 'matin-arbres', title: 'Matin des Arbres' }); });
+app.get('/ocean',                   function(req, res) { res.render('ocean',                    { page: 'ocean',        title: 'Océan' }); });
+app.get('/foret',                   function(req, res) { res.render('foret',                    { page: 'foret',        title: 'Forêt' }); });
+app.get('/chevaux',                 function(req, res) { res.render('chevaux',                  { page: 'chevaux',      title: 'Chevaux' }); });
+app.get('/contact',                 function(req, res) { res.render('contact',                  { page: 'contact',      title: 'Contact' }); });
 
 
 app.use(function(req, res, next) { res.render('404', { page: '404',  title: '404' }); });
